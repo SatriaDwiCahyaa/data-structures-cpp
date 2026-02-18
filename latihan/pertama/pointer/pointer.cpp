a1a099b80927fe4bd2628564f4c6c46c730dfe0d
SatriaDwiCahyaa <zatriagithub@gmail.com>
2026-02-18 14:27:26 +0700

    feat : tambah file pointer.cpp

---
+++ latihan/pertama/pointer/pointer.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+template <typename T>
+
+
+void print(T teks){
+    cout << teks << endl;
+}
+
+
+int main(){
+    string nama = "Satria";
+
+    print(nama);
+
+    
+    string* ptr= &nama;
+    print(ptr);
+    print(*ptr);
+
+    *ptr = "Dwi";
+    print(*ptr);
+    print(ptr);
+    
+}
